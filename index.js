4ab18c304bbc39ada4e4d2de643f438f549daf53
SU-T <guitaristpeet@gmail.com>
2022-09-11 21:44:11 -0700

    change shapes position

--- index.js
+++ index.js
@@ -1,68 +1,71 @@
-const sqTl = gsap.timeline({repeat: -1});
-sqTl.to('.square', {
-    x: '200%',
-    rotate: 90,
-    duration: 2.5,
-    ease: 'power2.inOut'
-})
-sqTl.to('.square', {
-    x: '0%',
-    rotate: 180,
-    duration: 2.5,
-    ease: 'power2.inOut'
-})
-sqTl.to('.square', {
-    x: '200%',
-    rotate: 270,
-    duration: 2.5,
-    ease: 'power2.inOut'
-})
-sqTl.to('.square', {
-    x: '0%',
-    rotate: 360,
-    duration: 2.5,
-    ease: 'power2.inOut'
-})
+// const sqTl = gsap.timeline({repeat: -1});
+// sqTl.to('.square', {
+//     x: '200%',
+//     rotate: 90,
+//     duration: 2.5,
+//     ease: 'power1.inOut'
+// })
+// sqTl.to('.square', {
+//     x: '0%',
+//     rotate: 180,
+//     duration: 2.5,
+//     ease: 'power1.inOut'
+// })
+// sqTl.to('.square', {
+//     x: '200%',
+//     rotate: 270,
+//     duration: 2.5,
+//     ease: 'power1.inOut'
+// })
+// sqTl.to('.square', {
+//     x: '0%',
+//     rotate: 360,
+//     duration: 2.5,
+//     ease: 'power1.inOut'
+// })
 
-const triTl = gsap.timeline({repeat: -1});
-triTl.to('.triangle', {
-    x: '-200%',
-    rotate: 90,
-    duration: 2.5,
-    ease: 'power2.inOut'
-})
-triTl.to('.triangle', {
-    x: '0%',
-    rotate: 180,
-    duration: 2.5,
-    ease: 'power2.inOut'
-}) 
-triTl.to('.triangle', {
-    x: '-200%',
-    rotate: 270,
-    duration: 2.5,
-    ease: 'power2.inOut'
-})
-triTl.to('.triangle', {
-    x: '0%',
-    rotate: 360,
-    duration: 2.5,
-    ease: 'power2.inOut'
-}) 
+// const triTl = gsap.timeline({repeat: -1});
+// triTl.to('.triangle', {
+//     x: '-200%',
+//     rotate: 90,
+//     duration: 2.5,
+//     ease: 'power1.inOut'
+// })
+// triTl.to('.triangle', {
+//     x: '0%',
+//     rotate: 180,
+//     duration: 2.5,
+//     ease: 'power1.inOut'
+// }) 
+// triTl.to('.triangle', {
+//     x: '-200%',
+//     rotate: 270,
+//     duration: 2.5,
+//     ease: 'power1.inOut'
+// })
+// triTl.to('.triangle', {
+//     x: '0%',
+//     rotate: 360,
+//     duration: 2.5,
+//     ease: 'power1.inOut'
+// }) 
 
-const circleTl =gsap.timeline({repeat: -1});
-circleTl.to('.circle', {
-    x: '100%',
-    duration: 1.45,
-    ease: 'power1.Out'
-})
-circleTl.to('.circle', {
-    x: '-100%',
-    duration: 2.1,
-    ease: 'power1.inOut'
-})
-circleTl.to('.circle', {
-    x: '0%',
-    duration: 1.45,
-    ease: 'power1.in'
-})
+// const circleTl =gsap.timeline({repeat: -1});
+// circleTl.to('.circle', {
+//     x: '100%',
+//     duration: 1.45,
+//     ease: 'power1.Out'
+//     // ease: 'linear'
+// })
+// circleTl.to('.circle', {
+//     x: '-100%',
+//     duration: 2.1,
+//     ease: 'power1.inOut'
+//     // ease: 'linear'
+// })
+// circleTl.to('.circle', {
+//     x: '0%',
+//     duration: 1.45,
+//     ease: 'power1.in'
+//     // ease: 'linear'
+// })
