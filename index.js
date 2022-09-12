2b10d02fdfe089560743c4789bc8e5649d4829de
SU-T <guitaristpeet@gmail.com>
2022-09-11 21:47:02 -0700

    change x value of triangle animation

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-const sqTl = gsap.timeline({repeat: -1});
-sqTl.to('.square', {
-    x: '140%',
-    rotate: 90,
-    duration: 2.5,
-    ease: 'power1.inOut'
-})
-sqTl.to('.square', {
-    x: '0%',
-    rotate: 180,
-    duration: 2.5,
-    ease: 'power1.inOut'
-})
-sqTl.to('.square', {
-    x: '140%',
-    rotate: 270,
-    duration: 2.5,
-    ease: 'power1.inOut'
-})
-sqTl.to('.square', {
-    x: '0%',
-    rotate: 360,
-    duration: 2.5,
-    ease: 'power1.inOut'
-})
-
-// const triTl = gsap.timeline({repeat: -1});
-// triTl.to('.triangle', {
-//     x: '-200%',
+// const sqTl = gsap.timeline({repeat: -1});
+// sqTl.to('.square', {
+//     x: '140%',
 //     rotate: 90,
 //     duration: 2.5,
 //     ease: 'power1.inOut'
 // })
-// triTl.to('.triangle', {
+// sqTl.to('.square', {
 //     x: '0%',
 //     rotate: 180,
 //     duration: 2.5,
 //     ease: 'power1.inOut'
-// }) 
-// triTl.to('.triangle', {
-//     x: '-200%',
+// })
+// sqTl.to('.square', {
+//     x: '140%',
 //     rotate: 270,
 //     duration: 2.5,
 //     ease: 'power1.inOut'
 // })
-// triTl.to('.triangle', {
+// sqTl.to('.square', {
 //     x: '0%',
 //     rotate: 360,
 //     duration: 2.5,
 //     ease: 'power1.inOut'
-// }) 
+// })
+
+const triTl = gsap.timeline({repeat: -1});
+triTl.to('.triangle', {
+    x: '-135%',
+    rotate: 90,
+    duration: 2.5,
+    ease: 'power1.inOut'
+})
+triTl.to('.triangle', {
+    x: '0%',
+    rotate: 180,
+    duration: 2.5,
+    ease: 'power1.inOut'
+}) 
+triTl.to('.triangle', {
+    x: '-135%',
+    rotate: 270,
+    duration: 2.5,
+    ease: 'power1.inOut'
+})
+triTl.to('.triangle', {
+    x: '0%',
+    rotate: 360,
+    duration: 2.5,
+    ease: 'power1.inOut'
+}) 
 
 // const circleTl =gsap.timeline({repeat: -1});
 // circleTl.to('.circle', {
