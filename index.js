057fe90db48b4651ef4de6e06d835cafff6bb0f7
SU-T <guitaristpeet@gmail.com>
2022-09-11 21:52:33 -0700

    change x value of circle animation

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// const sqTl = gsap.timeline({repeat: -1});
-// sqTl.to('.square', {
-//     x: '140%',
-//     rotate: 90,
-//     duration: 2.5,
-//     ease: 'power1.inOut'
-// })
-// sqTl.to('.square', {
-//     x: '0%',
-//     rotate: 180,
-//     duration: 2.5,
-//     ease: 'power1.inOut'
-// })
-// sqTl.to('.square', {
-//     x: '140%',
-//     rotate: 270,
-//     duration: 2.5,
-//     ease: 'power1.inOut'
-// })
-// sqTl.to('.square', {
-//     x: '0%',
-//     rotate: 360,
-//     duration: 2.5,
-//     ease: 'power1.inOut'
-// })
+const sqTl = gsap.timeline({repeat: -1});
+sqTl.to('.square', {
+    x: '140%',
+    rotate: 90,
+    duration: 2.5,
+    ease: 'power1.inOut'
+})
+sqTl.to('.square', {
+    x: '0%',
+    rotate: 180,
+    duration: 2.5,
+    ease: 'power1.inOut'
+})
+sqTl.to('.square', {
+    x: '140%',
+    rotate: 270,
+    duration: 2.5,
+    ease: 'power1.inOut'
+})
+sqTl.to('.square', {
+    x: '0%',
+    rotate: 360,
+    duration: 2.5,
+    ease: 'power1.inOut'
+})
 
 const triTl = gsap.timeline({repeat: -1});
 triTl.to('.triangle', {
@@ -50,22 +50,19 @@ triTl.to('.triangle', {
     ease: 'power1.inOut'
 }) 
 
-// const circleTl =gsap.timeline({repeat: -1});
-// circleTl.to('.circle', {
-//     x: '100%',
-//     duration: 1.45,
-//     ease: 'power1.Out'
-//     // ease: 'linear'
-// })
-// circleTl.to('.circle', {
-//     x: '-100%',
-//     duration: 2.1,
-//     ease: 'power1.inOut'
-//     // ease: 'linear'
-// })
-// circleTl.to('.circle', {
-//     x: '0%',
-//     duration: 1.45,
-//     ease: 'power1.in'
-//     // ease: 'linear'
-// })
+const circleTl =gsap.timeline({repeat: -1});
+circleTl.to('.circle', {
+    x: '70%',
+    duration: 1.45,
+    ease: 'power1.Out'
+})
+circleTl.to('.circle', {
+    x: '-70%',
+    duration: 2.1,
+    ease: 'power1.inOut'
+})
+circleTl.to('.circle', {
+    x: '0%',
+    duration: 1.45,
+    ease: 'power1.in'
+})
